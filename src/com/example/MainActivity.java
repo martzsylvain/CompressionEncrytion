0477ccb4d86c7c62a0b19b4b96c8647dbb71f335
Sylvain Martinez <martzsylvain@gmail.com>
2012-06-06 14:02:26 +0200

    Base64.java and Compression/decompression added

--- src/com/example/MainActivity.java
+++ src/com/example/MainActivity.java
@@ -2,6 +2,9 @@ package com.example;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity
 {
@@ -12,5 +15,10 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         Person person = new Person("Martinez", "Sylvain", "0553490732", "6 Allee du clot de pelle bourrut", "0987654321", "1234567890");
+        try {
+            CompressionDecompression.compress(person.getTel() + person.getAddress() + person.getNumBlueCard() + person.getNumHealthCard());
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
     }
 }
